5d38108f5a6eead40d1ce81e033ee1bff1c36e45
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-17 17:40:02 +0300

    commit tree efc967581b746d3ad0be41242d556bc74600b9c5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.chars.last == '.' ? true : false
+    # self.end_with?('.') ? true : false
   end
+  # "The weather outside is frightful.".sentence?
+  # # true
+  # "But the fire is so delightful".sentence?
+  # # false
+  #================================================================
 
   def question?
-
+    self.end_with?('?') ? true : false
   end
+  # "The weather outside is frightful?".question?
+  # # true
+  # "But the fire is so delightful.".question?
+  # # false
+  #================================================================
 
   def exclamation?
-
+    self.include?('!') ? true : false
   end
+  # "The weather outside is frightful?".exclamation?
+  # # true
+  # "But the fire is so delightful!".exclamation?
+  # # false
+  #================================================================
 
   def count_sentences
-
+    self.split(/\.|\!|\?/).find_all {|sentence| !sentence.empty?}.length
   end
+  #================================================================
+  "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
 end
